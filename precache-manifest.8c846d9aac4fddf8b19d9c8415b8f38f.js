47c65a727f7a602483158f28af0d2ab939bcba0c
unknown <m.hassan.rahi@gmail.com>
2020-03-24 15:24:31 +0500

    Updates

---
+++ precache-manifest.8c846d9aac4fddf8b19d9c8415b8f38f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8d1e8d0d300756e10aa734f31b6f391",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "8c1b4253428854b51d1a",
+    "url": "/monster-rolodex/static/css/main.b0b9290c.chunk.css"
+  },
+  {
+    "revision": "a7a85362896843811758",
+    "url": "/monster-rolodex/static/js/2.f99245a6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.f99245a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c1b4253428854b51d1a",
+    "url": "/monster-rolodex/static/js/main.35cd19ad.chunk.js"
+  },
+  {
+    "revision": "98c8b3d0c3ab21abc082",
+    "url": "/monster-rolodex/static/js/runtime-main.9ae1052e.js"
+  }
+]);
